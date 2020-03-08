2132a60214c503cdbf06867f9753706b5e8e8fb5
huukhangapcs <huukhangvn@gmail.com>
2020-03-08 11:18:38 +0700

    Second OpenCV lesson

---
+++ CaptureVideo.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+out = cv2.VideoWriter('output.avi', fourcc,24, (640,480))
+# file name or device index (0 or -1, 1,2)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    if ret == True:
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break    
+        
+cap.release()
+out.release()
+cv2.destroyAllWindows()
