dfff540569245a78f5a5655f12386ba2b4e8a35f
huukhangapcs <huukhangvn@gmail.com>
2020-03-09 08:35:58 +0700

    09/03/2020

--- CaptureVideo.py
+++ CaptureVideo.py
@@ -4,7 +4,8 @@ cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
 out = cv2.VideoWriter('output.avi', fourcc,24, (640,480))
 # file name or device index (0 or -1, 1,2)
-
+cap.set(3,1208)
+cap.set(4,720)
 while cap.isOpened():
     ret, frame = cap.read()
     #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
